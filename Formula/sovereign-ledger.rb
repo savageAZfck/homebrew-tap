8adc0aa96511d022d9d82e4dbce5303e87db2dbc
savag3 <savagetism@icloud.com>
2026-09-13 13:03:01 -0400

    sovereign-ledger 0.3.0 retag

--- Formula/sovereign-ledger.rb
+++ Formula/sovereign-ledger.rb
@@ -2,7 +2,7 @@ class SovereignLedger < Formula
   desc "Hardened hash-chained audit ledger: HMAC entries, Merkle proofs, Secure Enclave anchors"
   homepage "https://github.com/savageAZfck/sovereign_ledger"
   url "https://github.com/savageAZfck/sovereign_ledger/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "8a00f3ead03248dcd1f26b9296b7a3e3accdfb19aa241db88ae8558f4b9c4f46"
+  sha256 "cc7ed188a009a6134aba03dfc04d31b8c0cd7394b0ce942ba1df5d2626114c97"
   license :cannot_represent
 
   depends_on "rust" => :build
