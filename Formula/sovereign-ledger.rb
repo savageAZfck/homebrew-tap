d92d2dc774eb0b2e37e9055a15397c965a6d72cc
savag3 <savagetism@icloud.com>
2026-09-13 10:49:47 -0400

    Fix tarball sha256 (was computed while repo was private)

--- Formula/sovereign-ledger.rb
+++ Formula/sovereign-ledger.rb
@@ -2,7 +2,7 @@ class SovereignLedger < Formula
   desc "Hardened hash-chained audit ledger: HMAC entries, Merkle proofs, Secure Enclave anchors"
   homepage "https://github.com/savageAZfck/sovereign_ledger"
   url "https://github.com/savageAZfck/sovereign_ledger/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "dc63e5e97a5b1d412609ca6f1e27ddee3c0448204cd90f23a178288a7fad5e7e"
   license :cannot_represent
 
   depends_on "rust" => :build
